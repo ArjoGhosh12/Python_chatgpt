cbaf95379f2196df6d44743e010611fc775510ab
Arjo Ghosh <122384138+ArjoGhosh12@users.noreply.github.com>
2023-10-17 02:55:18 +0530

    Update 03 chatgpt chat assistant website.py

--- Include/03 chatgpt chat assistant website.py
+++ Include/03 chatgpt chat assistant website.py
@@ -1,7 +1,7 @@
 import openai
 import gradio
 
-openai.api_key = "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"
+openai.api_key = "#"
 
 messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
 
